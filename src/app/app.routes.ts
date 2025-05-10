9785679622060cee7ba5d7d9dc2d6617e516e9b4
Quillraven <quillraven@gmail.com>
2025-05-10 20:46:43 +0000

    Deploying to gh-pages from @ Quillraven/gdx-quilly-utils@6ad79da4d49ced883abb9c98758176659947faa7 🚀

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import {Routes} from '@angular/router';
-import {TileExtruderComponent} from './components/tile-extruder/tile-extruder.component';
-import {HomeComponent} from './components/home/home.component';
-
-export const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'tile-extruder', component: TileExtruderComponent},];
