baf0caedcb9d4e2fc8386ec7328d4b3651340020
Quillraven <quillraven@gmail.com>
2026-04-19 16:43:14 +0200

    update gdx kotlin template and kotlin version

--- src/app/components/gradle-kotlin-template/gradle-kotlin-template.component.ts
+++ src/app/components/gradle-kotlin-template/gradle-kotlin-template.component.ts
@@ -9,7 +9,7 @@ import JSZip from 'jszip';
 
 const FILES_TO_UPDATE = ['kt', 'kts', 'md'];
 const LINE_ENDING = '\n';
-const KOTLIN_DEFAULT_VERSION = '2.3.10';
+const KOTLIN_DEFAULT_VERSION = '2.3.20';
 
 @Component({
   selector: 'app-gradle-kotlin-template',
