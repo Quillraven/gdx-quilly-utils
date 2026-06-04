109375d7e46cd3948bff34488816697a1156de9c
Quillraven <quillraven@gmail.com>
2026-06-04 14:51:27 +0200

    update gdx-kotlin-template to latest version and update Kotlin default version to 2.4.0

--- src/app/components/gradle-kotlin-template/gradle-kotlin-template.component.ts
+++ src/app/components/gradle-kotlin-template/gradle-kotlin-template.component.ts
@@ -9,7 +9,7 @@ import JSZip from 'jszip';
 
 const FILES_TO_UPDATE = ['kt', 'kts', 'md'];
 const LINE_ENDING = '\n';
-const KOTLIN_DEFAULT_VERSION = '2.3.21';
+const KOTLIN_DEFAULT_VERSION = '2.4.0';
 
 @Component({
   selector: 'app-gradle-kotlin-template',
